109b08ffbf6e688c4005638b7f8b2af7df2402bf
Nicola Cuomo <nicola.cuomo@relatech.com>
2022-09-09 09:35:14 +0200

    typescript lint setup

---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+type HomeProps = {
+  allPostsData: { id: string, title: string, date: string }[],
+};
+
+export default function Home({ allPostsData }: HomeProps) {
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              <Link href={`/posts/${id}`}>{id}</Link>
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
